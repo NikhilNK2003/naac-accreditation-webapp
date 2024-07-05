a8789b23dc2eaf0982cf26eab1ece68e84d2d370
srikrishnag28 <srikrishnag28@gmail.com>
2024-07-06 02:08:00 +0530

    criteria 1.2.2 created

--- acc/criteria_1/urls.py
+++ acc/criteria_1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import render_temp
+from .views import *
 
 urlpatterns = [
-    path('temp/', render_temp)
+    path('temp/', render_temp),
+    path('1_2_2/', criteria_1_2_2, name='criteria_1_2_2'),
+    path('1_2_2/form/', criteria_1_2_2_form, name='criteria_1_2_2_form'),
 ]
--- acc/criteria_1/views.py
+++ acc/criteria_1/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 
 
 def render_temp(request):
-    return render(request, 'base.html')
+    return render(request, 'form/criteria_1_2_2.html')
+
+
+def criteria_1_2_2(request):
+    return render(request, 'table/criteria_1_2_2.html')
+
+def criteria_1_2_2_form(request):
+    return render(request, 'form/criteria_1_2_2.html')
