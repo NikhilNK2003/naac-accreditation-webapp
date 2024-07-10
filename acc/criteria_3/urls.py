3aa88af598c5b1d128610ef20fab5f71d083df7f
srikrishnag28 <srikrishnag28@gmail.com>
2024-07-10 19:53:39 +0530

    b

---
+++ acc/criteria_3/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('3_1_1/', criteria_3_1_1, name='criteria_3_1_1'),
+    path('3_1_1/form/', criteria_3_1_1_form, name='criteria_3_1_1_form'),
+    path('3_1_1/upload_excel/', upload_excel_3_1_1, name='upload_excel_3_1_1'),
+    path('3_1_1/export_excel/', export_excel_3_1_1, name='export_excel_3_1_1'),
+]
